a48abf17e0409ca7f7a6ef1fb8d852e5d2f4e68e
nguyentrongbut <cloly031128@gmail.com>
2025-03-02 23:04:27 +0700

    feat: authentication

--- middlewares/admin/auth.middlewares.js
+++ middlewares/admin/auth.middlewares.js
@@ -1,15 +1,21 @@
 const {prefixAdmin} = require("../../config/system");
 const Account = require("../../models/account.model")
+const Role = require("../../models/role.model")
 
 module.exports.requireAuth = async (req, res, next) => {
 
     if (!req.cookies.token) {
         res.redirect(`${prefixAdmin}/auth/login`);
     } else {
-        const user = await Account.findOne({ token: req.cookies.token })
+        const user = await Account.findOne({ token: req.cookies.token }).select("-password")
         if (!user) {
             res.redirect(`${prefixAdmin}/auth/login`);
         } else {
+            const role = await Role.findOne({_id: user.role_id}).select("title permissions")
+
+            res.locals.user = user;
+            res.locals.role = role;
+
             next()
         }
     }
